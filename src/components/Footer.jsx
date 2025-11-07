fa821c4fba7dcd7b2289afd22a8c9be2579df0a1
Cristobal <crispinochet@outlook.cl>
2025-11-07 20:29:33 -0300

    feat: Create Footer component with navigation and social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,20 @@
+import { Instagram } from 'lucide-react';
+
+
 function Footer() {
     return (
         <div>
-            <div className="container"> 
+            <div className="container bg-body-secondary"> 
                 <footer className="py-3 my-4"> 
+                    <h2>PanZano</h2>
                     <ul className="nav justify-content-center border-bottom pb-3 mb-3"> 
-                        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li> 
-                        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li> 
-                        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li> 
+                        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Inicio</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Productos</a></li> 
+                        <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Contacto</a></li> 
                         <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li> 
                     </ul> 
-                    <p className="text-center text-body-secondary">© 2025 Company, Inc</p> 
+                    <Instagram /> <a href="https://www.instagram.com/panzanonatural/">PanZano</a>
+                    <p className="text-center text-body-secondary">&copy; {new Date().getFullYear()} PanZano.</p> 
                 </footer> 
             </div>
         </div>
